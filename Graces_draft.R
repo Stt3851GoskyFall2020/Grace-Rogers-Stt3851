27a5ca99d643e507922228d95fba8165be4e6c99
Grace Rogers <rogersgrace24@gmail.com>
2020-04-01 16:48:47 -0400

    testing models

--- Graces_draft.R
+++ Graces_draft.R
@@ -9,10 +9,10 @@ library(skimr)
 library(car)
 library(standardize)
 Housing <- read_excel("Housing.xlsx")
-Housing$elemfactor <- as.numeric(as.factor(elem))
-Housing$statusfactor <- as.numeric(as.factor(status))
-Housing$edison <- ifelse(elem == 'edison', 1, 0)
-Housing$parker <- ifelse(elem == 'parker', 1, 0)
+Housing$elemfactor <- as.numeric(as.factor(Housing$elem))
+Housing$statusfactor <- as.numeric(as.factor(Housing$status))
+Housing$edison <- ifelse(Housing$elem == 'edison', 1, 0)
+Housing$parker <- ifelse(Housing$elem == 'parker', 1, 0)
 #size: pr = .081 R^2 = .04058
 #Lot: pr = .0335 R^2 = .05965
 #bath:pr = .131 R^2 = .03051
@@ -26,11 +26,33 @@ Housing$parker <- ifelse(elem == 'parker', 1, 0)
 #edison: pr = .0088, R^2 = .089
 #parker: pr = .0414 R^2 = .05501
 
+
 scaled_data <- data.frame(scale(Housing[,2:9]), scale(Housing[,12:15]))
+#R^2 : .4116
 Housing_model1_everything<-lm(price~ bedrooms+garagesize+elem+status,data=Housing)
 summary(Housing_model1_everything)
-Housing_model1_everything<-lm(price~ bedrooms+garagesize+elem+status,data=scaled_data)
+
+#R^2 : 0.5209
+Housing_model1_everything<-lm(price~ bedrooms+garagesize+elem:status,data=Housing)
 summary(Housing_model1_everything)
+
+#R^2 : 0.6127
+Housing_model1_everything<-lm(price ~ poly(lot,2)+ bedrooms + garagesize+ elem:status,data=Housing)
+summary(Housing_model1_everything)
+vif(Housing_model1_everything)
+
+#R^2 : 0.6124
+Housing_model1_everything<-lm(price ~ poly(lot,2)+ garagesize + elem:status, data=Housing)
+summary(Housing_model1_everything)
+plot(Housing_model1_everything)
+
+
+Housing_model1_everything<-lm(price~ bedrooms:garagesize+elem:status,data=Housing)
+summary(Housing_model1_everything)
+
+Housing_model1_everything<-lm(price~ bedrooms+elem:garagesize +status,data=Housing)
+summary(Housing_model1_everything)
+
 vif(Housing_model1_everything)
 plot(Housing_model1_everything)
 
